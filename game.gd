9deb7c6d2bf3a6b1c38a0805b6cc35330b84af94
direfiresmat <direfiresmat@gmail.com>
2026-07-23 14:30:25 -0400

    Game Over

--- game.gd
+++ game.gd
@@ -11,9 +11,19 @@ func start():
 
 func _on_hunger_test_pressed() -> void:
 	stats.hunger_decay()
+	hunger_update()
+	health_update()
 
-func hunger_update(_delta): 
-	$Label.text = stats.hunger
+func hunger_update(): 
+	$HungerUI.text = "Hunger: " + str(stats.hunger)
 
 func _on_timer_timeout() -> void:
 	print("Time Out")
+
+func health_update():
+		$HealthUI.text = "Health: " + str(stats.health)
+
+
+func _on_check_status_pressed() -> void:
+	health_update()
+	stats.check_status()
--- stats.gd
+++ stats.gd
@@ -7,8 +7,34 @@ var cold: int = 100
 var sleep: int = 100
 #Mechanics
 var hunger_reduction: float = 5
+var cold_reduction: float = 2.5
 var time: float = 250
 
 func hunger_decay():
-	hunger -= hunger_reduction
-	print(hunger)
+	check_status()
+	if hunger == 0:
+		health -= 10
+	if hunger != 0:
+		hunger -= hunger_reduction
+		print(hunger)
+		if hunger <= 0:
+			print ("starve")
+	check_status()
+func cold_decay():
+	check_status()
+	if cold == 0:
+		health -= 10
+	if cold != 0:
+		cold -= cold_reduction
+		print(cold)
+		if cold <= 0:
+			print ("freeze")
+	check_status()
+
+func check_status():
+	if health <= 0:
+		game_over()
+	
+func game_over():
+	get_tree().change_scene_to_file("res://Menu.tscn")
+	
