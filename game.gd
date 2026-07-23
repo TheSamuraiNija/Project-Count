b9682c294c35da5742d9c718d3a1781443019ef4
direfiresmat <direfiresmat@gmail.com>
2026-07-23 14:05:08 -0400

    Timer

--- game.gd
+++ game.gd
@@ -1,7 +1,19 @@
 extends Node2D
 
+func _ready():
+	print("Opened")
+	start()
+	
+func start():
+	$Timer.wait_time = stats.time
+	print($Timer.wait_time)
+	pass
+
 func _on_hunger_test_pressed() -> void:
 	stats.hunger_decay()
 
 func hunger_update(_delta): 
 	$Label.text = stats.hunger
+
+func _on_timer_timeout() -> void:
+	print("Time Out")
--- stats.gd
+++ stats.gd
@@ -1,9 +1,13 @@
 extends Node2D
-
+#Basic Player Stats
 var health: int = 100
 var sanity: int = 45
 var hunger: int = 100
+var cold: int = 100
+var sleep: int = 100
+#Mechanics
 var hunger_reduction: float = 5
+var time: float = 250
 
 func hunger_decay():
 	hunger -= hunger_reduction
